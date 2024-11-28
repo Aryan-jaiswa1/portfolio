c9d07607e4b6d53b2ac35a68517bb0156279290b
Aryan-jaiswa1 <aryan.jaiswal1114@gmail.com>
2024-11-28 20:10:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-let menu=document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+const navbarHTML = `
+    <ul class="navbar">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#services">Skills</a></li>
+        <li><a href="#plans">Projects</a></li>
+        <li><a href="#review">Contact</a></li>
+    </ul>
+`;
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
 
-window.onscroll = () => {
-    menu.classList.remove('bx-x'); 
-    navbar.classList.remove('active');
-
-}
-// Typing Text Code 
+    if (navbar.textContent.trim() === "") {
+        navbar.innerHTML = navbarHTML;
+    } else {
+        navbar.textContent = "";
+    }
+};
 const  typed = new Typed('.multiple-text', {
     strings: ['Maharana Pratap College of Professional Studies', 'BCA Student CGPA: 7.8'],
     typeSpeed: 60,
